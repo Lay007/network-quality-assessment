0b2eb3dd434accdd3eb47b57d895784551882af7
lay007 <laymob@gmail.com>
2019-07-25 01:16:51 +0300

    Update main.go
    edite metric

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ const (
 
 func main() {
     var metrics []*Metric
-    metrics = append(metrics, NewMetric("SFP-SLA_4401", "delay", "1.22", time.Now().Unix()))
+    metrics = append(metrics, NewMetric("SFP-SLA_4401", "delay", 4343)
     metrics = append(metrics, NewMetric("SFP-SLA_4401", "status", "OK"))
 
     // Create instance of Packet class
