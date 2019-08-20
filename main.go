666d9e5e91121b2cfdbd6ae5f81254cf8abe7c77
lay007 <laymob@gmail.com>
2019-08-20 11:37:33 +0300

    fix

--- main.go
+++ main.go
@@ -222,8 +222,8 @@ func receiveMessages(c net.PacketConn, mtu int) {
 			log.Fatalf("failed to unmarshal ethernet frame: %v", err)
 		}
 		fmt.Printf("\n\n--=Test %x - \n",f.Payload[12:16])
-		var ips []byte
-		copy(ips,(net.ParseIP(ipsrcstr)).To4())
+		var ips [4]byte
+		copy(ips[:],(net.ParseIP(ipsrcstr)).To4())
 		fmt.Printf("\n\n--=T_so %x - \n",ips)
 
 		// Display source of message and message itself.
