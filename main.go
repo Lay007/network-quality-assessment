a3d2cbb707295e111ed3a9bbbedc94ab33e8760d
lay007 <laymob@gmail.com>
2019-08-21 03:15:57 +0300

    fix jitter

--- main.go
+++ main.go
@@ -275,7 +275,7 @@ func getJitter(in_solve int64) float32 {
 
 	size_s = 100
 	mass_solve = append(mass_solve, in_solve)
-	if len(mass_solve) < size_s {
+	if len(mass_solve) < (size_s+1) {
 		return 0
 	}
 	mass_solve = mass_solve[1:(size_s + 1)]
