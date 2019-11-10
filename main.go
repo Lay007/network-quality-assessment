1fea059d2e17f2a28ff3e9c4b4ab8f077f5aac5f
lay007 <laymob@gmail.com>
2019-11-10 22:26:01 +0300

    add_diff

--- main.go
+++ main.go
@@ -14,9 +14,18 @@ import (
 
 	"github.com/mdlayher/ethernet"
 	"github.com/mdlayher/raw"
+
+	"database/sql"    
+    _ "github.com/go-sql-driver/mysql"
+
 )
 
-const (
+const (	
+	
+	db_user = `sfp_user`
+	db_user_pass= `rootsfp`
+    db_database = `server_sfp_sla`
+
 	debugV = true
 
 	defaultHost = `localhost`
@@ -51,6 +60,20 @@ type sfpsla struct {
 	number      uint32
 }
 
+type global_config struct{
+	server_ip 	string
+	zabbix_server_name string
+	vlan 		int
+	vlan_number int
+}
+type module_sfp struct {
+	id int
+	name string
+	address_mac string
+	address_ip string
+	version string
+	location string
+}
 var numberTx, numberCounter uint32
 
 func checksum(buf []byte) uint16 {
@@ -87,6 +110,47 @@ func (h *iphdr) checksum() {
 }
 
 func main() {
+
+	db, err := sql.Open("mysql", db_user + ":" + db_user_pass+"@/" + db_database)
+    if err != nil {
+        panic(err)
+    } 
+   
+    row, err := db.Query("select * from global_config")
+	if err != nil {
+        panic(err)
+    }
+    defer row.Close()
+	row.Next();
+	conf:=new(global_config)
+	err = row.Scan(&conf.server_ip, &conf.zabbix_server_name, &conf.vlan, &conf.vlan_number )
+	if err != nil{
+        fmt.Println(err)        
+    }
+	
+	rows, err := db.Query("select * from modules_sfp_sla")
+	if err != nil {
+        panic(err)
+    }
+	defer rows.Close()
+	
+	modules:=[]module_sfp{}
+
+	for rows.Next(){
+		m:=module_sfp{}
+		err = rows.Scan(&m.id, &m.name, &m.address_mac, &m.address_ip,&m.version, &m.location)
+        if err != nil{
+            fmt.Println(err)
+            continue
+		}
+		fmt.Println(m.address_ip)
+        modules = append(modules, m)
+	}
+
+
+	defer db.Close()
+
+		
 	//go zabbixHello("SFP-SLA_4401")
 	// Find all devices
 	devices, err := pcap.FindAllDevs()
