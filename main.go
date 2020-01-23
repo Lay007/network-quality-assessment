744bfaf00597260d73fcf5e372589026b544e590
Alexandr L <laymob@gmail.com>
2020-01-23 13:13:32 +0300

    repeat

--- main.go
+++ main.go
@@ -432,7 +432,7 @@ func TestThroughput(id int, net_interface_name string) { //Нагрузочно�
 //	var b_split = []byte {0, 0, 0}
 	//b_big :=make([]byte,(len(b)+3)*3)
 	var b_big []byte
-	b_big=bytes.Repeat(b,1)
+	b_big=bytes.Repeat(b,2)
 	//b_big = append(b_big,b_split)
 	//b_big=append(b_big,b_split)
 	//b_big=append(b_big,b_split)
