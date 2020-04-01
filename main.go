5177f822e0dfa348b530ef0833ea8f4bd3c2facb
Alexandr L <laymob@gmail.com>
2020-04-01 14:15:08 +0300

    time ti nano

--- main.go
+++ main.go
@@ -892,8 +892,10 @@ func packetForm(ipsrc net.IP, ipdst1 net.IP, ipdst2 net.IP, mac_src []byte, mac_
 	var ind uint
 	//ip.iplen = uint16(20 + 26 + 4)
 	if testWay == 2 {
-		t_time := int64(time.Now().Unix())
-		t_time = t_time << (4*8)
+		t_time := int64(time.Now().UnixNano())
+		//* 1000000 / float32(math.Pow(2, 32))
+
+		//t_time = t_time << (4*8)
 		for ind = 0; ind < 7; ind++ {
 			sfpdat.merkertime2[6-ind] = byte((t_time >> (8 * ind)) & 0xFF)
 		}
