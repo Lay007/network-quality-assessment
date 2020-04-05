3d1b726c1f2a2dc4f0f17a66751cd8e6c34588fd
Alexandr L <laymob@gmail.com>
2020-04-06 02:47:03 +0300

    add test

--- main.go
+++ main.go
@@ -1272,8 +1272,8 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 		if time.Since(start) > (time.Second * 20) {
 			break
 		}
-		//n, addr, err := c.ReadFrom(b)
-		n, _, err := c.ReadFrom(b)
+		n, addr, err := c.ReadFrom(b)
+		//n, _, err := c.ReadFrom(b)
 		if err != nil {
 			fmt.Printf("failed to receive message: %v", err)
 			log.Fatalf("failed to receive message: %v", err)
@@ -1301,28 +1301,28 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 		//fmt.Println(" --== End Test ==--")
 
 		// Display source of message and message itself.
-		if (len(f.Payload)>=52)&&(f.Payload[20] == 0xFC) && (bytes.Equal(f.Payload[12:16], ips[:]) == true) && (bytes.Equal(f.Payload[50:52], t_ips[:]) == true) {
+		if (len(f.Payload) >= 52) && (f.Payload[20] == 0xFC) && (bytes.Equal(f.Payload[12:16], ips[:]) == true) && (bytes.Equal(f.Payload[50:52], t_ips[:]) == true) {
 
 			(*test).number++
 			/*
-			fmt.Printf("\n\n--=Packet DETECT!!!=--\n")
-			//fmt.Printf("\n\n--=Test %x - \n -== %x\n",f.Payload[12:15],net.ParseIP(ipdst_1sfpsla_str))
-			fmt.Printf("size: %v raw:  %x \n", len(f.Payload), f.Payload)
-			fmt.Printf("\n\rEthernet source: [%s]\n", addr.String())
+				fmt.Printf("\n\n--=Packet DETECT!!!=--\n")
+				//fmt.Printf("\n\n--=Test %x - \n -== %x\n",f.Payload[12:15],net.ParseIP(ipdst_1sfpsla_str))
+				fmt.Printf("size: %v raw:  %x \n", len(f.Payload), f.Payload)
+				fmt.Printf("\n\rEthernet source: [%s]\n", addr.String())
 
-			fmt.Printf("size     %x \n", b[2:4])
+				fmt.Printf("size     %x \n", b[2:4])
 
-			fmt.Printf("ip sourse %v.%v.%v.%v \n", f.Payload[12], f.Payload[13], f.Payload[14], f.Payload[15])
-			fmt.Printf("ip dst    %v.%v.%v.%v \n", f.Payload[16], f.Payload[17], f.Payload[18], f.Payload[19])
+				fmt.Printf("ip sourse %v.%v.%v.%v \n", f.Payload[12], f.Payload[13], f.Payload[14], f.Payload[15])
+				fmt.Printf("ip dst    %v.%v.%v.%v \n", f.Payload[16], f.Payload[17], f.Payload[18], f.Payload[19])
 
-			fmt.Printf("ip SFP2   %v.%v.%v.%v \n", f.Payload[21], f.Payload[22], f.Payload[23], f.Payload[24])
+				fmt.Printf("ip SFP2   %v.%v.%v.%v \n", f.Payload[21], f.Payload[22], f.Payload[23], f.Payload[24])
 
-			fmt.Printf("time marker_SFP1_1 :   %x \n", f.Payload[25:32])
-			fmt.Printf("time marker_SFP2   :   %x \n", f.Payload[32:39])
-			fmt.Printf("time marker_SFP1_2 :   %x \n", f.Payload[39:46])
-			fmt.Printf("Number marker      :   %x \n", f.Payload[46:50])
-			fmt.Println(" --== End Packet ==--")
-*/
+				fmt.Printf("time marker_SFP1_1 :   %x \n", f.Payload[25:32])
+				fmt.Printf("time marker_SFP2   :   %x \n", f.Payload[32:39])
+				fmt.Printf("time marker_SFP1_2 :   %x \n", f.Payload[39:46])
+				fmt.Printf("Number marker      :   %x \n", f.Payload[46:50])
+				fmt.Println(" --== End Packet ==--")
+			*/
 			var markerSFP11, markerSFP12, markerSFP2 int64
 			var ind uint
 
@@ -1360,7 +1360,7 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 			if test_id.test_loss == true {
 				loss = zabbix_error(node_zabbix, float32(numberR-test.number)/float32(numberR), host_zabbix, port_zabbix)
 			}
-/*
+			//*
 			if test_id.test_type == 1 {
 				if test_id.test_delay_1 == true {
 					rez_delay := (*test).getOneDelay(markerSFP2 - markerSFP11)
@@ -1387,7 +1387,7 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 					}
 				}
 			}
-*/
+			//*/
 			db, err := sql.Open("mysql", db_user+":"+db_user_pass+"@/"+db_database)
 			if err != nil {
 				db.Close()
@@ -1434,10 +1434,10 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 			}
 
 			db.Close()
-			fmt.Printf("  ==>> %V  --> %s\n",id,time.Since(start))
+			fmt.Printf("  ==>> %v  --> %s\n", id, time.Since(start))
 			break
 		} else {
-			//	fmt.Printf("\n\n\r[%s] %v %x", addr.String(), len(f.Payload), f.Payload[:25])
+			fmt.Printf("\n\n\r[%s] %v %x", addr.String(), len(f.Payload), f.Payload[:25])
 
 		}
 	}
