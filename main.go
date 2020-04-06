d7b9e32d3f0e835f0accd8fb68dc031e24b74d94
Alexandr L <laymob@gmail.com>
2020-04-06 23:33:40 +0300

    fixx

--- main.go
+++ main.go
@@ -1273,11 +1273,14 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 
 		}
 
+		if (n+16) != packetSize {
+			continue
+		}
+
 		if time.Since(start) > (time.Second * 20) {
 			break
 		}
-		
-		
+				
 		t_time := int64(float64(time.Now().UnixNano())*float64(math.Pow(2, 32)/1000000000)) - 0x55817800000000
 		t_time = t_time & int64(0xFFFFFFFFFFFFFF)
 
@@ -1287,10 +1290,8 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 			fmt.Printf("failed to unmarshal ethernet frame: %v", err)
 			log.Fatalf("failed to unmarshal ethernet frame: %v", err)
 		}
-		fmt.Printf(" ==> n = %v, packedSize= %v",len(f.Payload),packetSize)
-		if len(f.Payload) != packetSize {
-			continue
-		}
+		//fmt.Printf(" ==> n = %v, packedSize= %v",len(f.Payload),packetSize)
+		
 		//fmt.Println("\n\n--=Test ==-- - ")
 		//fmt.Printf("\n\rEthernet source: [%s]\n", addr.String())
 		//fmt.Printf("ip sourse %v.%v.%v.%v \n", f.Payload[12], f.Payload[13], f.Payload[14], f.Payload[15])
