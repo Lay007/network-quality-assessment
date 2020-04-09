84366d2a1b08002eaec75fa7a457412a3f5d1253
Alexandr L <laymob@gmail.com>
2020-04-09 14:40:42 +0300

    find

--- main.go
+++ main.go
@@ -903,7 +903,7 @@ func packetForm(ipsrc net.IP, ipdst1 net.IP, ipdst2 net.IP, mac_src []byte, mac_
 		//	t_time := int64(float64(time.Now().UnixNano())*float64(math.Pow(2, 32)/1000000000))
 		//t_time = t_time << (4*8)
 		delta_nano := int64((2208988800) * 1000000000)
-		t_time := int64(float64(time.Now().UnixNano() - delta_nano))
+		t_time := int64(float64(time.Now().UnixNano()-delta_nano) * float64(math.Pow(2, 32)/1000000000))
 		t_time = t_time & int64(0xFFFFFFFFFFFFFF)
 
 		for ind = 0; ind < 7; ind++ {
@@ -1322,7 +1322,7 @@ func (test *testSLA) receiveMessages(id int, c net.PacketConn, ipdst_1sfpsla_str
 
 		//t_time := int64(float64(time.Now().UnixNano() - delta_nano )*float64(math.Pow(2, 32)/1000000000)) - 0x55817F00000000
 		delta_nano := int64((2208988800) * 1000000000)
-		t_time := int64(float64(time.Now().UnixNano() - delta_nano))
+		t_time := int64(float64(time.Now().UnixNano()-delta_nano) * float64(math.Pow(2, 32)/1000000000))
 		t_time = t_time & int64(0xFFFFFFFFFFFFFF)
 
 		//n, addr, err := c.ReadFrom(b)
