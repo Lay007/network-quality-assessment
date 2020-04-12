7e35af54a572de8b652bff5d26dc37739c9d5a54
Alexandr L <laymob@gmail.com>
2020-04-13 02:17:00 +0300

    find

--- testThroughput.go
+++ testThroughput.go
@@ -490,7 +490,7 @@ func (test *testThr) testThrGen(net_interface_name string, b []byte, c *raw.Conn
 	//	bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: uint32(test_type), SkipTrue: 1},
 		// Выбор одного из 1000
 		bpf.LoadExtension{Num: bpf.ExtRand},
-		bpf.JumpIf{Cond: bpf.JumpLessThan, Val: (2 ^ 32)/100, SkipFalse: 1},
+		bpf.JumpIf{Cond: bpf.JumpLessThan, Val: 0xFF, SkipFalse: 1},
 		// Verdict is "send up to 4k of the packet to userspace."
 		bpf.RetConstant{Val: 4096},
 		// Verdict is "ignore packet."
