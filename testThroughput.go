abaf338027abc0c3aae4d64f78defdb58774a679
Alexandr L <laymob@gmail.com>
2020-04-13 02:06:27 +0300

    filter

--- testThroughput.go
+++ testThroughput.go
@@ -483,14 +483,14 @@ func (test *testThr) testThrGen(net_interface_name string, b []byte, c *raw.Conn
 
 	(*netConfRecive).Filter, _ = bpf.Assemble([]bpf.Instruction{
 		// Проверка идентификатора пакета (34 бит) (xFA-от 1 ко 2, xFB – от 2 к 1, xFC – от 1 к Серверу)
-		bpf.LoadAbsolute{Off: 34, Size: 1},
-		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: 0xFC, SkipTrue: 3},
+	//	bpf.LoadAbsolute{Off: 34, Size: 1},
+	//	bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: 0xFC, SkipTrue: 3},
 		// Проверка идентификатора теста
-		bpf.LoadAbsolute{Off: 64, Size: 2},
-		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: uint32(test_type), SkipTrue: 1},
+	//	bpf.LoadAbsolute{Off: 64, Size: 2},
+	//	bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: uint32(test_type), SkipTrue: 1},
 		// Выбор одного из 1000
-	//	bpf.LoadExtension{Num: bpf.ExtRand},
-	//	bpf.JumpIf{Cond: bpf.JumpLessThan, Val: (2 ^ 32)/1000, SkipFalse: 1},
+		bpf.LoadExtension{Num: bpf.ExtRand},
+		bpf.JumpIf{Cond: bpf.JumpLessThan, Val: (2 ^ 32)/100, SkipFalse: 1},
 		// Verdict is "send up to 4k of the packet to userspace."
 		bpf.RetConstant{Val: 4096},
 		// Verdict is "ignore packet."
