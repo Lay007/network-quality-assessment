f5db8503311e98bad75e1ced6e93859fb27260ed
Alexandr L <laymob@gmail.com>
2020-04-13 05:51:34 +0300

    find timer

--- timerReal.go
+++ timerReal.go
@@ -13,8 +13,8 @@ func (t *TimerR) timerDelayNano(delay int64) {
 	countTimer := delay / t.minPeriodNano
 	var x int64
 	var ind int64
-	for ind = 0; ind < countTimer; ind++ {
-		k := float64(ind * ind)
+	for ind = 1; ind < countTimer; ind++ {
+		k := float64(ind * ind)/float64(ind)
 		x = x * int64(k)*(x+123)
 	}
 
