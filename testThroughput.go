d3a4ed259181c82ff7ad78ed5f4709c827ca4b2e
Alexandr L <laymob@gmail.com>
2020-04-16 12:40:32 +0300

    socket

--- testThroughput.go
+++ testThroughput.go
@@ -410,7 +410,7 @@ func (test *testThr) testThrGen(net_interface_name string, b []byte, c *raw.Conn
 	if err != nil {
 		return 0, 0
 	}
-//ifi.Index
+	//ifi.Index
 	test_count := 10000
 	gen_test_pps_start := time.Now()
 	for {
@@ -558,6 +558,8 @@ func (test *testThr) testThrGen(net_interface_name string, b []byte, c *raw.Conn
 
 		counter.Set(cnt)
 
+		genSocket(ifi.Index, b)
+
 		for i := 0; i < K; i++ {
 			go func() {
 
@@ -582,7 +584,7 @@ func (test *testThr) testThrGen(net_interface_name string, b []byte, c *raw.Conn
 						break ExitLoop
 					default:
 						n, err := con.WriteTo(b, addr)
-						genSocket(ifi.Index,b)
+						
 						if addDelay {
 							timerReal.timerDelayNano(period_nano)
 						}
@@ -669,7 +671,7 @@ func (test *testThr) receivePackets(c net.PacketConn, mtu int, ipdst_1sfpsla_str
 	}
 }
 
-func genSocket(ifiIndex int,b []byte){
+func genSocket(ifiIndex int, b []byte) {
 
 	zs, err := zsocket.NewZSocket(ifiIndex, zsocket.ENABLE_RX, 2048, 64, nettypes.All)
 	// the above will result in a ring buffer of 64 frames at
@@ -679,49 +681,55 @@ func genSocket(ifiIndex int,b []byte){
 		panic(err)
 	}
 
-	for ind:=0;ind<128;ind++{
-		zs. WriteToBuffer(b,64)
+	for ind := 0; ind < 128; ind++ {
+		tx, err := zs.WriteToBuffer(b, 64)
+		fmt.Println(" -- Socket_Generaror --")
+		fmt.Println(" -- >> Tx = ", tx)
+		fmt.Println(" -- >> Error = ", err)
+
 	}
-zs.FlushFrames()
-	
+	fl_l, err, err_sl := zs.FlushFrames()
+	fmt.Println(" -- >> Flash Tx = ", fl_l)
+	fmt.Println(" -- >> Error = ", err)
+	fmt.Println(" -- >> Error slice = ", err_sl)
 
 	//var conn poll.FD
 	// MyCon := syscall.Socket()
-//var	 socket net.Conn
-//MyConn, _:= rawsocketcall()
+	//var	 socket net.Conn
+	//MyConn, _:= rawsocketcall()
 
-/*
-fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_IP)
-if err != nil {
-	fmt.Println(err)
-}
+	/*
+	   fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_IP)
+	   if err != nil {
+	   	fmt.Println(err)
+	   }
 
-file := os.NewFile(uintptr(fd), "")
+	   file := os.NewFile(uintptr(fd), "")
 
-for {
-	buffer := make([]byte, 1024)
-	num, _ := file.Write(buffer)
+	   for {
+	   	buffer := make([]byte, 1024)
+	   	num, _ := file.Write(buffer)
 
-	fmt.Printf("% X\n", buffer[:num])
-}
+	   	fmt.Printf("% X\n", buffer[:num])
+	   }
 
-// Called in init() in package raw
-/*
-net.RegisterSocket(
-	syscall.AF_PACKET,
-	&syscall.SockaddrLinklayer{},
-	&Addr{},
-	// internal conversion functions for syscall.SockaddrLinklayer <-> raw.Addr
-	convertSockaddr,
-	convertNetAddr,
-	)
-	sock, _ := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, proto)
-	_ = syscall.Bind(sock, &syscall.SockaddrLinklayer{
-	Protocol: pbe,
-	Ifindex: ifi.Index,
-	})
-	f := os.NewFile(uintptr(sock), "linklayer")// c is type net.SocketConn, backed by raw socket (uses raw.Addr for addressing)c := net.FilePacketConn(f)
-*/
+	   // Called in init() in package raw
+	   /*
+	   net.RegisterSocket(
+	   	syscall.AF_PACKET,
+	   	&syscall.SockaddrLinklayer{},
+	   	&Addr{},
+	   	// internal conversion functions for syscall.SockaddrLinklayer <-> raw.Addr
+	   	convertSockaddr,
+	   	convertNetAddr,
+	   	)
+	   	sock, _ := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, proto)
+	   	_ = syscall.Bind(sock, &syscall.SockaddrLinklayer{
+	   	Protocol: pbe,
+	   	Ifindex: ifi.Index,
+	   	})
+	   	f := os.NewFile(uintptr(sock), "linklayer")// c is type net.SocketConn, backed by raw socket (uses raw.Addr for addressing)c := net.FilePacketConn(f)
+	*/
 }
 
 /*
