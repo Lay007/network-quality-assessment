3141d5b412b55c1384e8bf653dffb3d787253915
Alexandr L <laymob@gmail.com>
2020-04-17 03:31:46 +0300

    find

--- zsocket/zsocket.go
+++ zsocket/zsocket.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"unsafe"
 	"golang.org/x/sys/unix"
+
 	"github.com/newtools/zsocket/inet"
 	"github.com/newtools/zsocket/nettypes"
 )
@@ -274,11 +275,11 @@ func (zs *ZSocket) MaxPackets() int32 {
 }
 
 func (zs *ZSocket) SetMAX()  {
-	if errReusePort := SetsockoptInt(zs.socket, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); errReusePort != nil {
+	if errReusePort := syscall.SetsockoptInt(zs.socket, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); errReusePort != nil {
 		fmt.Printf("reuse port error: %v\n", errReusePort)
 		return
 	}
-	if errReusePort := SetsockoptInt(zs.socket, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); errReusePort != nil {
+	if errReusePort := syscall.SetsockoptInt(zs.socket, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); errReusePort != nil {
 		fmt.Printf("reuse port error: %v\n", errReusePort)
 		return
 	}
