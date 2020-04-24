f13ca87bf64293e95b1664da6b9512815e1361b9
Alexandr L <laymob@gmail.com>
2020-04-24 20:02:38 +0300

    fix

--- requestSNMP.go
+++ requestSNMP.go
@@ -20,7 +20,7 @@ func findSFP(ip_1sfpsla_str string, ip_2sfpsla_str string) bool {
 
 	oids := []string{".1.3.6.1.4.1.2010.1.13.0", ".1.3.6.1.4.1.2010.1.14.0"}
 
-	timer_SNMP := time.NewTicker(1000 * time.Millisecond)
+	timer_SNMP := time.NewTicker(100 * time.Millisecond)
 	for range timer_SNMP.C {
 		result, err2 := g.Default.Get(oids) // Get() accepts up to g.MAX_OIDS
 		if err2 != nil {
@@ -30,9 +30,6 @@ func findSFP(ip_1sfpsla_str string, ip_2sfpsla_str string) bool {
 
 		for i, variable := range result.Variables {
 			fmt.Printf("%d: oid: %s ", i, variable.Name)
-
-			// the Value of each variable returned by Get() implements
-			// interface{}. You could do a type switch...
 			switch variable.Type {
 			case g.OctetString:
 				fmt.Printf("string: %s\n", string(variable.Value.([]byte)))
@@ -40,7 +37,9 @@ func findSFP(ip_1sfpsla_str string, ip_2sfpsla_str string) bool {
 				// ... or often you're just interested in numeric values.
 				// ToBigInt() will return the Value as a BigInt, for plugging
 				// into your calculations.
-				fmt.Printf("number: %v   Byte/s\n", g.ToBigInt(variable.Value)) //*8)/1000000)
+				val := g.ToBigInt(variable.Value)
+				//val.Int64()
+				fmt.Printf("number: %v   Mb/s\n", val.Int64()*8 / 1000.0)
 			}
 		}
 	}
