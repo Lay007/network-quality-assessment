f6358e14dc581cfd79be5fd65a310cfcdf7c42c2
lay007 <laymob@gmail.com>
2020-06-24 03:45:49 +0700

    fix

--- testDelay.go
+++ testDelay.go
@@ -597,7 +597,7 @@ func (test *testDelay) receiveMessagesDelay(catchDetect chan int64, c net.Packet
 				if floatDelay < 25 {
 
 					fmt.Printf("size     %x \n", b[2:4])
-					fmt.Println("Packet: ",f.Payload)
+					fmt.Printf("Packet: %x ",f.Payload)
 					fmt.Printf("ip sourse %v.%v.%v.%v \n", f.Payload[12], f.Payload[13], f.Payload[14], f.Payload[15])
 					fmt.Printf("ip dst    %v.%v.%v.%v \n", f.Payload[16], f.Payload[17], f.Payload[18], f.Payload[19])
 
