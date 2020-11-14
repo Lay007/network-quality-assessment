0116de87ed04422c495cbed91f3a7be0e1a7f123
lay007 <laymob@gmail.com>
2020-11-14 18:16:42 +0700

    edite

--- configdb.sh
+++ configdb.sh
@@ -4,17 +4,16 @@ echo " =>> MySQL config"
 mysql -u root -psecret <<MY_QUERY
 
 CREATE DATABASE IF NOT EXISTS server_sfp_sla;
-TRUNC Table server_sfp_sla
+DROP DATABASE server_sfp_sla;
+CREATE DATABASE server_sfp_sla;
 USE server_sfp_sla;
 SOURCE ./server_sfp_sla.sql;
 
-GRANT USAGE ON *.* TO 'sfp_user'@'localhost';
-DROP user 'sfp_user'@'localhost';
-
-flush privileges;
-
 USE mysql;
+drop user 'sfp_user'@'localhost';
+flush privileges;
 create user 'sfp_user'@'localhost' identified by 'rootsfp';
+
 grant all privileges on server_sfp_sla . * to 'sfp_user'@'localhost';
 
 MY_QUERY
@@ -25,6 +24,6 @@ echo " =>> MySQL config succes"
 
 echo " =>> Deploy web-config"
 
-tar -C / -xvf httpServerSLA.tar.gz
+#tar -C / -xvf httpServerSLA.tar.gz
 
 echo " =>> Deploy web-config succes"
